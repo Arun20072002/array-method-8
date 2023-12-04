6b2e07954467c9a7c2874a670967ce34d509dc28
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 11:33:53 +0530

    Array-method-8

---
+++ Array-method-8.js
@@ -0,0 +1,8 @@
+document.write("8.Replace the values css with bootstrap at index 1"+"<br>")
+document.write("--------------------------------------------------"+"<br>")
+
+var language = ['html', 'css', 'js', 'reactjs'];
+document.write(language+"<br>")
+
+language.splice(1,1,"bootstrap")
+document.write(language+"<br>")
